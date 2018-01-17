aca2c51bc5bdbad76f33a275b30c33999791b953
technowizard <aheisleycook@gmail.com>
2018-01-17 10:49:15 -0800

    added gui and oop practice
    will add more

---
+++ main.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from tkinter import ttk
+from comtypes.client import CreateObject
+from comtypes.gen import SpeechLib
+import logging
+from tkinter import messagebox as mbx
+"""sets up initial conversion"""
+class MainApplication(tk.Frame):
+    def __init__(self,*args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        """application configuration"""
+        menu = tk.Menu()
+        root.grid_rowconfigure(2, weight=2)
+       
+        root.config(menu=menu)
+        root.resizable(0,0)
+        root.title("my converter")
+        root.geometry("200x200")
+        menu.add_command(label="exit", command=root.quit)   
+        root.configure(background="green")
+        menu.configure(background="white")
+        self.Linput = ttk.Label(root, text="enter a conversion source", width=30)
+        self.Linput.pack()
+        self.inputentry = ttk.Entry(root, width=20)
+        self.inputentry.pack()
+        self.Loutput = ttk.Label(text="enter a conversion destination", width=20)
+        self.Loutput.grid_rowconfigure(4, weight=2)
+        self.Loutput.pack()
+        self.OutputEntry = ttk.Entry(width=20)
+        self.OutputEntry.pack()
+        self.Convert = ttk.Button(text="convert", command=self.ConvertertoAudio)
+        self.Convert.pack(side="top")
+
+    def ConvertertoAudio(self):
+        try: 
+            self.engine = CreateObject("SAPI.SpVoice")
+            self.stream = CreateObject("SAPI.SpFileStream")
+            self.infile = self.inputentry.get()
+            self.outfile = self.OutputEntry.get()
+            self.stream.Open(self.outfile, SpeechLib.SSFMCreateForWrite)
+            self.engine.AudioOutputStream = self.stream
+            with open(self.infile, "r") as text:
+                self.content = text.read()
+                self.engine.speak(self.content)
+                self.stream.Close()
+                mbx.showinfo(f" conversion of {self.infile} {self.outfile} (self.infile, self.outfile")
+
+        except Exception as e:
+            mbx.showwarning("error", e)
+          
+
+root =  tk.Tk()
+app = MainApplication()
+root.mainloop()
+
+
+
+
+
+
+
+
