4db838ebc91d12782acc2441a50b81cbb67cf2b6
technowizard <aheisleycook@gmail.com>
2018-01-22 18:05:08 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
 from comtypes.client import CreateObject
-from comtypes.gen import SpeechLib
 import logging
 from tkinter import messagebox as mbx
 """sets up initial conversion"""
+
+
 class MainApplication(tk.Frame):
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         """application configuration"""
         menu = tk.Menu()
         root.grid_rowconfigure(2, weight=2)
-       
+
         root.config(menu=menu)
-        root.resizable(0,0)
+        root.resizable(0, 0)
         root.title("my converter")
         root.geometry("200x200")
-        menu.add_command(label="exit", command=root.quit)   
+        menu.add_command(label="exit", command=root.quit)
         root.configure(background="green")
         menu.configure(background="white")
         self.Linput = ttk.Label(root, text="input", width=20)
@@ -28,10 +29,13 @@ class MainApplication(tk.Frame):
         self.Loutput.pack()
         self.OutputEntry = ttk.Entry(width=20)
         self.OutputEntry.pack()
-        self.Convert = ttk.Button(text="convert", command=self.ConvertertoAudio)
+        self.Convert = ttk.Button(
+        text="convert", command=self.ConverterToAudio)
         self.Convert.pack(side="top")
-"""sets up main function"""
-    def ConvertertoAudio(self):
+
+
+        """sets up main function"""
+    def ConverterToAudio(self):
         try: 
             self.engine = CreateObject("SAPI.SpVoice")
             self.stream = CreateObject("SAPI.SpFileStream")
