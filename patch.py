eba819765b5c78772d5f95771ef9ab46e201661e
technowizard <aheisleycook@gmail.com>
2018-02-01 10:25:48 -0800

    new update

--- patch.py
+++ patch.py
@@ -1,4 +1,3 @@
-
 import tkinter as tk
 #if your using python 2.xx
 # import Tkinter as tk
@@ -16,17 +15,19 @@ class MainApplication(tk.Frame):
         """application configuration"""
         menu = tk.Menu()
         root.grid_rowconfigure(2, weight=2)
-        
+        style = ttk.Style()
+        style.configure("TLabel", foreground="black", background="lightgreen")
+        style.configure("TButton", foreground="green", background="lightgreen")
         root.config(menu=menu)
         root.resizable(0, 0)
         root.title("my converter")
-        root.geometry("300x500")
+        root.geometry("500x300")
         menu.add_command(label="exit", command=root.quit)
         menu.add_command(label="help", command=self.showhelp)
         menu.add_command(label="about", command=self.about)
-        root.configure(background="green")
+        root.configure(background="lightgreen")
         menu.configure(background="white")
-        self.basic = ttk.Label(text="please click the help button first")
+        self.basic = ttk.Label(text="please click the help menubutton first you use  the application \n" )
         self.basic.pack()
         self.Linput = ttk.Label(root, text="input file", width=30)
         self.Linput.pack()
@@ -58,9 +59,11 @@ class MainApplication(tk.Frame):
             mbx.showwarning("error", e)
       
     def showhelp(self):
-        mbx.showinfo("help", "to use this application make sure you type in the file or directory you need converted in the input and output entries for example text.txt and then in the entry do text.mp3")
+        mbx.showinfo("help", "to use this application make sure you type in the file or directory you need converted in the input and output entries for example text.txt and then in the output entry do text.mp3")
     def about(self):
         mbx.showinfo("about", "\n\n created by austin heisley-cook\ndate 2/1/2018")
 root =  tk.Tk()
 app = MainApplication()
 root.mainloop()
+
+
