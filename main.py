12b0d567df3de9953382c91cdd6c3e51059f6845
technowizard <aheisleycook@gmail.com>
2018-02-17 23:42:10 -0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+
 import tkinter as tk
+#if your using python 2.xx
+# import Tkinter as tk
 from tkinter import ttk
 from comtypes.client import CreateObject
 from comtypes.gen import SpeechLib
@@ -6,34 +9,37 @@ import logging
 from tkinter import messagebox as mbx
 """sets up initial conversion"""
 
-
 class MainApplication(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         """application configuration"""
         menu = tk.Menu()
         root.grid_rowconfigure(2, weight=2)
-
+        style = ttk.Style()
+        style.configure("TLabel", relief="raised", foreground="black", background="lightgreen")
+        style.configure("TButton", foreground="grey", background="lightgreen", width=30)
         root.config(menu=menu)
         root.resizable(0, 0)
         root.title("my converter")
-        root.geometry("200x200")
+        root.geometry("500x200")
         menu.add_command(label="exit", command=root.quit)
-        root.configure(background="green")
+        menu.add_command(label="help", command=self.showhelp)
+        menu.add_command(label="about", command=self.about)
+        root.configure(background="lightgreen")
         menu.configure(background="white")
-        self.Linput = ttk.Label(root, text="input", width=20)
+        self.basic = ttk.Label(text="please click the help menu button first before you use  the application \n" )
+        self.basic.pack()
+        self.Linput = ttk.Label(root, text="input file", width=30)
         self.Linput.pack()
-        self.inputentry = ttk.Entry(root, width=20)
+        self.inputentry = ttk.Entry(root, width=30)
         self.inputentry.pack()
-        self.Loutput = ttk.Label(text="output", width=20)
+        self.Loutput = ttk.Label(text="output file", width=30)
         self.Loutput.grid_rowconfigure(4, weight=2)
         self.Loutput.pack()
-        self.OutputEntry = ttk.Entry(width=20)
+        self.OutputEntry = ttk.Entry(width=30)
         self.OutputEntry.pack()
         self.Convert = ttk.Button(text="convert", command=self.ConverterToAudio)
         self.Convert.pack(side="top")
-
-
         """sets up main function"""
     def ConverterToAudio(self):
         try: 
@@ -48,11 +54,13 @@ class MainApplication(tk.Frame):
                 self.engine.speak(self.content)
                 self.stream.Close()
                 mbx.showinfo("output", f" conversion of {self.infile} {self.outfile}")
-
         except Exception as e:
-            mbx.showwarning(e, "the file you destination is already converted")
-            
-
+            mbx.showwarning("error", e)
+      
+    def showhelp(self):
+        mbx.showinfo("help", "to use this application make sure you type in the file or directory you need converted in the input and output entries for example text.txt and then in the output entry do text.mp3 you can also tab to the tnext entry")
+    def about(self):
+        mbx.showinfo("about", "\n\n created by austin heisley-cook\ndate 2/1/2018")
 root =  tk.Tk()
 app = MainApplication()
 root.mainloop()
