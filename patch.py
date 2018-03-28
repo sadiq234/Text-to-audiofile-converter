29f3660432b44418fb7772a5e321f5ff880d1bd8
technowizard <aheisleycook@gmail.com>
2018-03-27 18:46:48 -0700

    added voice speed and pitch
    will add voice selection

--- patch.py
+++ patch.py
@@ -2,8 +2,8 @@ import tkinter as tk
 #if your using python 2.xx
 # import Tkinter as tk
 from tkinter import ttk
-#from comtypes.client import CreateObject
-#from comtypes.gen import SpeechLib
+from comtypes.client import CreateObject
+from comtypes.gen import SpeechLib
 import logging
 from tkinter import messagebox as mbx
 """sets up initial conversion"""
@@ -38,6 +38,10 @@ class MainApplication(tk.Frame):
         self.Loutput.pack()
         self.OutputEntry = ttk.Entry(width=30)
         self.OutputEntry.pack()
+        self.Lspeed = ttk.Label(text="set voice rate", width=30)
+        self.Lspeed.pack() 
+        self.Setrate = ttk.Entry()
+        self.Setrate.pack()
         self.Convert = ttk.Button(text="convert", command=self.ConverterToAudio)
         self.Convert.pack(side="top")
         """sets up main function"""
@@ -51,10 +55,12 @@ class MainApplication(tk.Frame):
             self.engine.AudioOutputStream = self.stream
             with open(self.infile, "r") as text:
                 self.content = text.read()
+                self.engine.rate = int(self.Setrate.get())
+                self.engine.pitch =int(self.Setrate.get())
                 self.engine.speak(self.content)
                 self.stream.Close()
                 #mbx.showinfo("output", f" conversion of {self.infile} {self.outfile}")
-                mbx.showinfo("output", "conversion of {} to {}".format(self.infile, self.outifle))
+                mbx.showinfo("output", "conversion of {} to {}".format(self.infile, self.outfile))
         except Exception as e:
             mbx.showwarning("error", e)
       
@@ -62,6 +68,7 @@ class MainApplication(tk.Frame):
         mbx.showinfo("help", "to use this application make sure you type in the file or directory you need converted in the input and output entries for example text.txt and then in the output entry do text.mp3 you can also tab to the tnext entry")
     def about(self):
         mbx.showinfo("about", "\n\n created by austin heisley-cook\ndate 2/1/2018")
+
 root =  tk.Tk()
 app = MainApplication()
 root.mainloop()
